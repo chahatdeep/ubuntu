fd052f6d4e6734f365236bdda08520b3e0942a1c
Chahat Deep Singh <chahatdeepsingh@gmail.com>
2016-10-09 02:55:29 -0400

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -4,10 +4,10 @@
 # By Chahat Deep Singh
 # Date: 09-Oct-2016
 
-# Installing Youtube Player ATRACI
-sudo add-apt-repository ppa:webupd8team/atraci
-sudo apt-get update
-sudo apt-get install atraci
+# Installing Youtube Player ATRACI [Discontinued; Search for Kaku for Youtube Linux API]
+#sudo add-apt-repository ppa:webupd8team/atraci
+#sudo apt-get update
+#sudo apt-get install atraci
 
 # Installing spotify
 sudo apt-get install spotify

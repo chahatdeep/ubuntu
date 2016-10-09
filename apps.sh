2d6324ace33c767f91c6104c05150ee5163b767f
Chahat Deep Singh <chahatdeepsingh@gmail.com>
2016-10-09 02:18:17 -0400

    Create apps.sh

---
+++ apps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# A shell script for ubuntu to install third party applications for more featured ubuntu installation.
+# By Chahat Deep Singh
+# Date: 09-Oct-2016
+
+# Installing Youtube
+sudo apt-get install mpv
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get update
+sudo apt-get install youtube-viewer
+
+#Installing 
